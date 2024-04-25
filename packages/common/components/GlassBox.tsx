fa6f9b9df4bab2669986d91619aa8b566eb2ae65
Marcin Ciarka <marcin@oazoapps.com>
2024-04-26 01:48:38 +0200

    Update GlassBox styles and separate database package

--- packages/common/components/GlassBox.tsx
+++ packages/common/components/GlassBox.tsx
@@ -1,9 +1,12 @@
 import { Box, BoxComponentProps } from "@mantine/core";
 import { PropsWithChildren } from "react";
 
+// Below errors are ignored, we need to `tsc` these files
+// just to get the types, bundling is actually made in /apps/web
+// and nextjs there handles it properly
 // @ts-ignore
 import * as glassBoxClasses from "./GlassBox.module.css";
-
+// @ts-ignore
 export const glassBoxClassName = glassBoxClasses.glassBoxClass;
 
 export const GlassBox = (props: PropsWithChildren<BoxComponentProps>) => (
